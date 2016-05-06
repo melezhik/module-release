86881e025170651483c491f55c4b846eb9250946
Alexey Melezhik <melezhik@gmail.com>
2016-05-06 14:39:06 +0300

    ignore these files

--- story.bash
+++ story.bash
@@ -1,5 +1,12 @@
-#set -x
-cd $project_root_dir
-carton exec release $(config options) $(config local_file) $(config remote_file)
+cd $test_root_dir
+
+touch releaserc
+
+export PERL5LIB=$project_root_dir/local/lib/perl5
+export PATH=$project_root_dir/local/bin
+
+
+release $(config options) $(config local_file) $(config remote_file)
+
 echo done
 
